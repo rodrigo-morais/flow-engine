60d3aaa839d9e4f341fed82c7dadb9dabcf6a176
rodrigo-morais <morais.rm@gmail.com>
2017-09-09 11:37:38 +0200

    Creating a Form component and adding to Register component.

--- app/components/Register/index.js
+++ app/components/Register/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import H1 from 'components/H1';
+import Form from 'components/Form';
 import messages from './messages';
 
 const Register = () => (
-  <H1>
-    <FormattedMessage {...messages.header} />
-  </H1>
+  <div>
+    <H1>
+      <FormattedMessage {...messages.header} />
+    </H1>
+    <Form>
+    </Form>
+  </div>
 );
 
 export default Register;
--- app/components/Register/tests/index.test.js
+++ app/components/Register/tests/index.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { shallow } from 'enzyme';
 
+import Form from 'components/Form';
 import Register from '../index';
 import messages from '../messages';
+
 
 describe('<Register />', () => {
   it('should render the page title', () => {
@@ -12,6 +14,15 @@ describe('<Register />', () => {
     );
     expect(renderedComponent.contains(
       <FormattedMessage {...messages.header} />
+    )).toEqual(true);
+  });
+
+  it('should contain a Form', () => {
+    const renderedComponent = shallow(
+      <Register />
+    );
+    expect(renderedComponent.contains(
+      <Form />
     )).toEqual(true);
   });
 });
